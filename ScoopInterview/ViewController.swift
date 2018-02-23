73a7ff6df11b044fceba58b502609cccbca7fd84
Luke Solomon <luke_solomon@icloud.com>
2018-02-23 12:46:42 -0800

    I should've been committing, but I guess I just like living dangerously.

--- ScoopInterview/ViewController.swift
+++ ScoopInterview/ViewController.swift
@@ -7,19 +7,33 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        label.text = ""
+        self.activityIndicator.startAnimating()
+        
+        Alamofire.request("https://api.takescoop.com/system/info").responseJSON { (data) -> Void in
+            let json = JSON(data.result.value)
+            let resData = Response.init(json: json)
+            
+            DispatchQueue.main.async(execute: {
+                self.activityIndicator.isHidden = true
+                self.label.text = "\(resData.flag)"
+            })
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
